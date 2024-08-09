137127aa20052a4fbcabb52ea70f852dd9abc814
Mariusz Borowiak <142698842+borowiak-m@users.noreply.github.com>
2024-08-09 10:08:51 +0100

    subs logic start

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,65 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/borowiak-m/sys-info-goth/internal/hardware"
+	"nhooyr.io/websocket"
 )
 
 type server struct {
-	buffer int
-	mux    http.ServeMux
+	buffer      int
+	mux         http.ServeMux
+	subscribers map[*subscriber]struct{}
+}
+
+type subscriber struct {
+	msgs chan []byte
 }
 
 func NewServer() *server {
 	s := &server{
-		buffer: 10,
+		buffer:      10,
+		subscribers: make(map[*subscriber]struct{}),
 	}
 
 	s.mux.Handle("/", http.FileServer(http.Dir("./htmx")))
 
 	return s
 }
+
+func (s *server) subscribeHandler(w http.ResponseWriter, req *http.Request) {
+	err := s.subscribe(req.Context(), w, req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func (s *server) subscribe(ctx context.Context, w http.ResponseWriter, req *http.Request) error {
+	var c *websocket.Conn
+	subscriber := &subscriber{
+		msgs: make(chan []byte, s.buffer),
+	}
+	s.addSubscriber(subscriber)
+
+	c, err := websocket.Accept(w, req, nil)
+	if err != nil {
+		return err
+	}
+	defer c.CloseNow()
+
+	ctx = c.CloseRead(ctx)
+	for {
+		// add sending messages to subs
+	}
+}
+
+func (s *server) addSubscriber(sub *subscriber) // to do
 
 func main() {
 	fmt.Println("Starting system monitor...")
